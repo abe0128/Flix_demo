6ff215762d61a6447e3cddca9c20788cc2066d23
Abraham De Alba <deal1211@m506-118-26434.local>
2018-09-11 00:52:36 -0700

    movie thing done

---
+++ Flix_demo/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flix_demo
+//
+//  Created by Abraham De Alba on 9/10/18.
+//  Copyright © 2018 Abraham De Alba. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterimageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
