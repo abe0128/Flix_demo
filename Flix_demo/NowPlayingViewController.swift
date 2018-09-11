b7e995118f7b0ff6e8177eafa5a7e75c23c45edb
Abraham De Alba <deal1211@m506-118-26434.local>
2018-09-10 22:44:54 -0700

    second flix submit

--- Flix_demo/NowPlayingViewController.swift
+++ Flix_demo/NowPlayingViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/{movie_id}?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        
+        tableView.dataSource = self
+        
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -25,13 +30,29 @@ class NowPlayingViewController: UIViewController {
             }
             else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
             }
         }
         task.resume()
         
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+       return cell
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
