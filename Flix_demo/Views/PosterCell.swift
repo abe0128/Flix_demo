f37ad1c086450f584aad6a427d01eefa4de5af34
Abraham De Alba <deal1211@m506-118-26434.local>
2018-09-19 09:51:44 -0700

    more stuff

---
+++ Flix_demo/Views/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  Flix_demo
+//
+//  Created by Abraham De Alba on 9/19/18.
+//  Copyright © 2018 Abraham De Alba. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
